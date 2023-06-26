bdebc5883191bcfca825f922162be58cf5a2e10e
ergincetn01 <ergincetn01@gmail.com>
2023-06-26 14:58:41 +0300

    req. func added

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ function App() {
         const reset=()=>{
             setText("")
         }
+
+        const timeRequired=()=> {
+            let timeReq=0
+            // within this function, calculate how much time required to read the given input text and show user.
+            return timeReq; 
+        }
     return (
         <div>
             <TextBox handleChange={handleChange} text={text}/>
             <button onClick={reset}>Reset text </button>
        <p>Text: {text}</p>
         Text len: {text.length}
+        Time Required to read this text: {timeRequired()}
         </div>
     )
 
